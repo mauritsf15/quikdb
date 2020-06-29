e55518cace204bec0a07baecccdca57c71142776
mauritsf <57948944+mauritsf15@users.noreply.github.com>
2020-06-29 16:21:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def store_a_value():
                 data = TinyWebDB(tag=tag, value=value)
                 db.session.add(data)
                 db.session.commit()
-        return jsonify(action="STORED", tag=tag, value=value)
+        return jsonify(success=True, tag=tag, value=value)
     return return_error('Please specify a tag name.')
 
 @app.route('/get', methods=['POST'])
@@ -64,10 +64,10 @@ def get_value():
                     return return_error('Inccorect password!')
             # --------------------
             value = TinyWebDB.query.filter_by(tag=tag).first().value
-            return jsonify(action="GOT", tag=tag, value=value)
+            return jsonify(success=True, tag=tag, value=value)
     return return_error('Could not find the tag.')
 
-@app.route('/auth/data', methods=['POST'])
+@app.route('/all', methods=['POST'])
 def get_data():
     getpassword = TinyWebDB.query.filter_by(tag='dbpass').first()
     if getpassword:
@@ -81,10 +81,10 @@ def get_data():
         for tg in tags:
             if tg.tag != 'dbpass':
                 datalist.append([tg.tag, tg.value])
-        return jsonify(action="DATA", data=datalist)
+        return jsonify(success=True, data=datalist)
     return return_error('Please set a password first.')
 
-@app.route('/getall', methods=['POST'])
+@app.route('/alltags', methods=['POST'])
 def get_all():
     getpassword = TinyWebDB.query.filter_by(tag='dbpass').first()
     if getpassword:
@@ -98,7 +98,7 @@ def get_all():
     for tg in tags:
     	if tg.tag != 'dbpass': 
             taglist.append(tg.tag)
-    return jsonify(action="TAGS", tag=taglist)
+    return jsonify(success=True, tag=taglist)
 
 @app.route('/delete', methods=['POST'])
 def delete_entry():
@@ -117,10 +117,10 @@ def delete_entry():
             deleted = TinyWebDB.query.filter_by(tag=tag).first()
             db.session.delete(deleted)
             db.session.commit()
-            return jsonify(action="DELETED", tag=tag)
+            return jsonify(success=True, tag=tag)
     return return_error('Could not find the tag.')
 
-@app.route('/format', methods=['POST'])
+@app.route('/reset', methods=['POST'])
 def delete_all():
     getpassword = TinyWebDB.query.filter_by(tag='dbpass').first()
     if getpassword:
@@ -132,12 +132,12 @@ def delete_all():
     try:
         count = db.session.query(TinyWebDB).delete()
         db.session.commit()
-        return jsonify(action="FORMATTED", count=count)
+        return jsonify(success=True, count=count)
     except:
         db.session.rollback()
         return return_error('Something went wrong while performing this action.')
 
-@app.route('/auth/password', methods=['POST'])
+@app.route('/setpass', methods=['POST'])
 def set_key():
     newpassword = request.form['newpass']
     getpassword = TinyWebDB.query.filter_by(tag='dbpass').first()
@@ -147,17 +147,17 @@ def set_key():
             if getpassword.value == oldpassword:
                 getpassword.value = newpassword
                 db.session.commit()
-                return jsonify(action="CHANGED PASSWORD", oldpass=oldpassword, newpass=newpassword)
+                return jsonify(success=True, oldpass=oldpassword, newpass=newpassword)
             else:
                 return return_error('Incorrect old password.')
         else:
             data = TinyWebDB(tag='dbpass', value=newpassword)
             db.session.add(data)
             db.session.commit()
-            return jsonify(action="SET PASSWORD", newpass=newpassword)
+            return jsonify(success=True, newpass=newpassword)
     return return_error('Please specify a new password.')
 
-@app.route('/auth/unlock', methods=['POST'])
+@app.route('/removepass', methods=['POST'])
 def remove_key():
     password = request.form['pass']
     getpassword = TinyWebDB.query.filter_by(tag='dbpass').first()
@@ -166,21 +166,21 @@ def remove_key():
             deleted = TinyWebDB.query.filter_by(tag='dbpass').first()
             db.session.delete(deleted)
             db.session.commit()
-            return jsonify(action="DELETED PASSWORD", password=password)
+            return jsonify(success=True, password=password)
         else:
         	return return_error('Incorrect password.')
     return return_error('There is no password to delete. Set a password first.')
 
-@app.route('/istrue', methods=['POST'])
+@app.route('/checkpass', methods=['POST'])
 def is_true():
     password = request.form['pass']
     getpassword = TinyWebDB.query.filter_by(tag='dbpass').first()
     if getpassword:
         # --------------------
         if password != getpassword.value:
-            return jsonify(action="IS CORRECT",result=False)
+            return jsonify(success=True,result=False)
         # --------------------
-    return jsonify(action="IS CORRECT",result=True)
+    return jsonify(success=True,result=True)
         
 
 @app.route('/count')
@@ -190,35 +190,35 @@ def count_all():
     resl = len(tags)
     if getpassword:
     	resl = resl - 1
-    return jsonify(action="COUNT", count=resl)
+    return jsonify(success=True, count=resl)
 
 @app.route('/version')
 def version():
-    return jsonify(action="VERSION", result="1.0")
+    return jsonify(success=True, result="1.0")
 
 @app.route('/islocked')
 def is_locked():
     getpassword = TinyWebDB.query.filter_by(tag='dbpass').first()
     if getpassword:
-        return jsonify(action="IS LOCKED",result=True)
+        return jsonify(success=True,result=True)
     else:
-        return jsonify(action="IS LOCKED",result=False)
+        return jsonify(success=True,result=False)
 
 @app.errorhandler(405)
 def method_not_allowed(e):
-    return jsonify(action="ERROR",result="Method is not allowed."), 405
+    return jsonify(success=False,result="Method is not allowed."), 405
 
 @app.errorhandler(404)
 def not_found(e):
-    return jsonify(action="ERROR",result="The requested URL was not found."), 404
+    return jsonify(success=False,result="The requested URL was not found."), 404
 
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return jsonify(action="ERROR",result="Internal server error."), 500
+    return jsonify(success=False,result="Internal server error."), 500
 
 def return_error(message):
-    response = jsonify(action="ERROR",result=message)
+    response = jsonify(success=False,result=message)
     response.status_code = 400
     return response
         
